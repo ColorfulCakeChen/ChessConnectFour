074baf05190d1fff34a77f88e72d232ef6e80fc3
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-01-08 13:28:29 +0800

    Update relay-sw.js
    Prefix precache file name with jsdelivr origin, path and version.

--- relay-sw.js
+++ relay-sw.js
@@ -5,3 +5,38 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+const sourceMeta = {
+  origin:         "https://cdn.jsdelivr.net",
+  pathFolderName: "/gh/ColorfulCakeChen/ChessConnectFour",
+  version:        "0.1"
+};
+
+/**
+ *
+ * @return Array The file names prefixed with sourceMeta.
+ */
+function produceSourceFileList(sourceMeta, fileNames) {
+  let result = new Array(fileNames.length);
+  let i = 0;
+  for (let fileName of fileNames) {
+    result[i] = sourceMeta.origin + sourceMeta.pathFolderName + "@" + sourceMeta.version + fileName;
+    ++i;
+  }
+  return result;
+}
+
+let precacheFileNames = produceSourceFileList(sourceMeta, [
+  "/index.html"
+]);
+
+workbox.precaching.precacheAndRoute(
+  precacheFileNames,
+  {
+    urlManipulation: ({url}) => {
+      ...
+      return [alteredUrlOption1, alteredUrlOption2, ...];
+    }
+  }
+);
+              
