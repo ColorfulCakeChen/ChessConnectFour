3fac06fc63e431cf74820dd2f260f83f6b2a3092
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-01-11 12:45:35 +0800

    Update relay-sw.js
    Replace class SourceMeta with JsDelivrPlugin by using replacePatternGitHubPages and replaceContextJsdelivr.

--- relay-sw.js
+++ relay-sw.js
@@ -6,20 +6,37 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-class SourceMeta {
+const sourceVersion = "0.16";
+
+/**
+ * 
+ */
+class JsDelivrPlugin {
+
   constructor(clientOriginPathFolderName, sourceOriginPathFolderName, version) {
+
+//!!! ...should be removed...
     this.clientOriginPathFolderName = clientOriginPathFolderName;
     this.sourceOriginPathFolderName = sourceOriginPathFolderName;
+
     this.version = version;
 
+//!!! ...should be removed...
     this.sourceOriginPathFolderVersionName = this.sourceOriginPathFolderName + "@" + this.version;
+
+    this.replacePatternGitHubPages = /^https:\/\/([^.]+)\.github\.io\/([^/]+)/; // e.g. https://colorfulcakechen.github.io/ChessConnectFour
+    this.replaceContextJsdelivr = "https://cdn.jsdelivr.net/gh/$1/$2";          // e.g. https://cdn.jsdelivr.net/gh/ColorfulCakeChen/ChessConnectFour
+    if (this.version)
+       this.replaceContextJsdelivr += "@" + this.version;                       // e.g. https://cdn.jsdelivr.net/gh/ColorfulCakeChen/ChessConnectFour@0.16
   }
 
+//!!! ...should be removed...
   /** @return {string} sourceOriginPathFolderVersionName + slashPathFileName. */
   prepend(slashPathFileName) {
     return this.sourceOriginPathFolderVersionName + slashPathFileName;
   }
 
+//!!! ...should be removed...
   /** @return {Array} The file names prefixed with prepend(). */
   prependList(slashPathFileNames) {
     let result = new Array(slashPathFileNames.length);
@@ -32,13 +49,73 @@ class SourceMeta {
     return result;
   }
 
+//!!! ...should be removed...
   /** @return {string} Replace clientOriginPathFileName by sourceOriginPathFolderVersionName. */
   replaceClientBySource(pathFileName) {
-    return pathFileName.replace(this.clientOriginPathFolderName, this.sourceOriginPathFolderVersionName);
+    //return pathFileName.replace(this.clientOriginPathFolderName, this.sourceOriginPathFolderVersionName);
+    return pathFileName.replace(this.replacePatternGitHubPages, this.replaceContextJsdelivr);
   }
+
+  /**
+   * Convert MIME type of .html file from text/plain to text/html.
+   */
+  async cacheWillUpdate({request, response, event}) {
+    // Return `response`, a different Response object or null
+    return response;
+  }
+
+//   async cacheDidUpdate({cacheName, request, oldResponse, newResponse, event}) {
+//     // No return expected
+//     // Note: `newResponse.bodyUsed` is `true` when this is called,
+//     // meaning the body has already been read. If you need access to
+//     // the body of the fresh response, use a technique like:
+//     // const freshResponse = await caches.match(request, {cacheName});
+//   }
+
+//   async cachedResponseWillBeUsed({cacheName, request, matchOptions, cachedResponse, event}) {
+//     // Return `cachedResponse`, a different Response object or null
+//     return cachedResponse;
+//   }
+
+  /**
+   * Replace path of GitHub Pages to jsdrlivr.
+   */
+  async requestWillFetch({request}) {
+    const newURL = request.url.replace(this.replacePatternGitHubPages, this.replaceContextJsdelivr);
+    if (request.url == newURL)
+      return request;  // If no replacement, no need to redirect.
+
+    // The following redirect method codes copied from:
+    // https://stackoverflow.com/questions/34640286/how-do-i-copy-a-request-object-with-a-different-url/34641566#34641566
+
+    const body = await ( request.headers.get('Content-Type') ? request.blob() : Promise.resolve(undefined) );
+    const newRequest = new Request(newURL, {
+        method: request.method,
+        headers: request.headers,
+        body: body,
+        referrer: request.referrer,
+        referrerPolicy: request.referrerPolicy,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        integrity: request.integrity,
+      })
+    );
+
+    return newRequest;  // Redirect to different URL.
+  }
+
+//   async fetchDidFail({originalRequest, request, error, event}) {
+//     // No return expected.
+//     // NOTE: `originalRequest` is the browser's request, `request` is the
+//     // request after being passed through plugins with
+//     // `requestWillFetch` callbacks, and `error` is the exception that caused
+//     // the underlying `fetch()` to fail.
+//   }
 }
 
-const sourceMeta = new SourceMeta(
+const theJsDelivrPlugin = new JsDelivrPlugin(
   "https://colorfulcakechen.github.io/ChessConnectFour",
   "https://cdn.jsdelivr.net/gh/ColorfulCakeChen/ChessConnectFour",
   "0.16"
@@ -49,7 +126,7 @@ const sourceMeta = new SourceMeta(
  * @return {Array} The URL list.
  */
 function urlManipulator({url}) {
-  let newURLString = sourceMeta.replaceClientBySource(url.href);
+  let newURLString = theJsDelivrPlugin.replaceClientBySource(url.href);
   let newURL = new URL(newURLString);
   let result = [newURL];
   console.log(`Convert "${url}" to "${newURL.href}"`);
@@ -80,12 +157,11 @@ let precacheFileNames = sourceMeta.prependList([
 
 workbox.precaching.precache(precacheFileNames);
 
-let routePattern = '${sourceMeta.clientOriginPathFolderName}.*';
 workbox.routing.registerRoute(
-  new RegExp(routePattern),
+  sourceMeta.replacePatternGitHubPages,
   workbox.strategies.cacheFirst({
     plugins: [
-//      new workbox.broadcastUpdate.Plugin('api-updates')
+      theJsDelivrPlugin
     ]
   })
 );
